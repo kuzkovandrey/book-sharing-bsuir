0c2d53cd9eb59267f48cf3ef2dd82aee567acb48
AndreyKuzkov <akuzkov@viju.ru>
2022-10-21 21:37:38 +0300

    Setup dependencies

--- apps/book-sharing/src/app/app.module.ts
+++ apps/book-sharing/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TuiAlertModule, TuiDialogModule, TuiRootModule } from '@taiga-ui/core';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
@@ -7,7 +8,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TuiRootModule,
+    TuiAlertModule,
+    TuiDialogModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
