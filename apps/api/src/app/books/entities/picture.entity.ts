e326948d94f841e8f390cd929390038fac8b7c47
kuzkovandrey <kuzkovandrey@gmail.com>
2022-11-11 22:03:28 +0300

    Fix bug with entity relation

--- apps/api/src/app/books/entities/picture.entity.ts
+++ apps/api/src/app/books/entities/picture.entity.ts
@@ -1,9 +1,13 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 
 import { BaseEntity, TableNames } from '@core';
+import { BookEntity } from './book.entity';
 
 @Entity({ name: TableNames.PICTURES })
 export class PictureEntity extends BaseEntity {
   @Column({ nullable: false, type: 'varchar' })
   url: string;
+
+  @ManyToOne(() => BookEntity, (book) => book.pictures)
+  book: BookEntity;
 }
